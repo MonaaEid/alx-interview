e6c8784c34f4d38bff6c9ef1c8766fc25062b70f
MonaaEid <monaa_eid@hotmail.com>
2024-04-26 04:25:36 +0300

    Add MINOPERATIONS

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations needed to result in exactly n H characters in the file"""
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
