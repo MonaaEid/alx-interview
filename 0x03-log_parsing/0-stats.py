42a7ab10d09f5c6d97446d4e9f7de525a489faaa
MonaaEid <monaa_eid@hotmail.com>
2024-05-03 03:52:50 +0300

    Add get methods

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """script that reads stdin line by line and computes metrics"""
-
-# Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
-
-
 import sys
 
 
