4d55272fe9b4e38a03e601e32755bd9c31abbfef
MonaaEid <monaa_eid@hotmail.com>
2024-05-10 02:16:28 +0300

    Validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,7 +8,7 @@ def validUTF8(data):
     count = 0
     for byte in data:
         if count == 0:
-            if (byte >> 5) == 0b110:
+            if (byte >> 5) == 0b110 or byte >> 5 == 0b11110:
                 count = 1
             elif (byte >> 4) == 0b1110:
                 count = 2
