c4a3a86b9d5d5bd24df2a8106afb7edaa429b076
MonaaEid <monaa_eid@hotmail.com>
2024-05-23 22:43:57 +0300

    Add api dir

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,34 @@
 #!/usr/bin/node
+// https://swapi-api.alx-tools.com/
+// Write a script that prints all characters of a Star Wars movie:
+
+// The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+// Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+// You must use the Star wars API
+// You must use the request module
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/' + process.argv[2];
-request(url, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(JSON.parse(body).name);
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/films/${movieId}`;
+function getCharacters (url) {
+    request(url, async function (error, response, body) {
+        if (error) {
+        console.log(error);
+        } else {
+        const characters = JSON.parse(body).characters;
+        for (const character of characters) {
+            await new Promise((resolve, reject) => {
+            request(character, function (error, response, body) {
+                if (error) {
+                reject(error);
+                } else {
+                console.log(JSON.parse(body).name);
+                resolve();
+                }
+            });
+            });
+        }
+        }
+    });
     }
-    }
-);
+getCharacters(url);
