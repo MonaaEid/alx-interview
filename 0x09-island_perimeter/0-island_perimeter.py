8178d879988c56b13b88d571c0aa00725e7d53f7
MonaaEid <monaa_eid@hotmail.com>
2024-06-13 02:58:37 +0300

    test

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """method that returns the perimeter of the island described in grid"""
+
+
 def island_perimeter(grid):
     """Return the perimeter of the island"""
     perimeter = 0
