9543e0888937589c6c42adf0d1a9b4203b2b0907
MonaaEid <monaa_eid@hotmail.com>
2024-06-20 07:21:55 +0300

    Add prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,7 +7,9 @@ def isWinner(x, nums):
     if x == 0 or x == 1:
         return None
     if x == 2:
-        return "Ben"
+        return "Maria"
     if x == 3:
         return "Maria"
-    return "Maria" if sum([1 for i in nums if i % 2 == 0]) == 0 else "Ben"
+    return "Maria" if sum([1 for n in nums if n % 2 == 0]) == 0 else "Ben"
+
+print("Winner: {}".format(isWinner(3, [1, 2, 3])))
