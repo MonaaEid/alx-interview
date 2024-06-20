0380ae75ae24c826c3253e3dfb2acad3a7d22049
MonaaEid <monaa_eid@hotmail.com>
2024-06-20 22:41:01 +0300

    Add dirs

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,18 +14,18 @@ def isPrime(n):
             return False
     return True
 
-def primeGame(n, nums):
+def isWinner(x, nums):
     """Prime Game"""
-    if n == 0:
+    if x == 0:
         return None
-    if n == 1:
+    if x == 1:
         return "Maria"
-    if n == 2:
+    if x == 2:
         return "Ben"
-    if n == 3:
+    if x == 3:
         return "Maria"
 
-    return "Maria" if sum([1 for n in nums if isPrime(n)]) % 2 == 0 else "Ben"
+    return "Maria" if sum([1 for x in nums if isPrime(x)]) % 2 == 0 else "Ben"
 
 
 # def isWinner(x, nums):
