ed07c7ba5b8828806dc557d2b57b10a0b2870892
MonaaEid <monaa_eid@hotmail.com>
2024-06-20 07:29:08 +0300

    Add prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,9 +7,15 @@ def isWinner(x, nums):
     if x == 0 or x == 1:
         return None
     if x == 2:
-        return "Maria"
+        return "Ben"
     if x == 3:
         return "Maria"
-    return "Maria" if sum([1 for n in nums if n % 2 == 0]) == 0 else "Ben"
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
 
 print("Winner: {}".format(isWinner(3, [1, 2, 3])))
